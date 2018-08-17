b79804fe401dc1c9b5b6c789aae1dc36186aa624
mikeyim337 <mk337337@gmail.com>
2018-08-17 14:09:55 -0700

    add functionality added

---
+++ public/api/post/add_item.php
@@ -0,0 +1,29 @@
+<?PHP
+
+$title = $_POST['title'];
+$details = $_POST['details'];
+
+if(empty($title)) {
+    $output['errors'][] = 'No title found';
+}
+
+if(empty($details)) {
+    $output['errors'][] = 'No details found';
+}
+
+
+if(empty($output['errors'])) {
+
+    $query = "INSERT INTO `items` (`title`, `details`) VALUES ('$title', '$details')";
+}
+
+$result = mysqli_query($conn, $query); 
+
+if(mysqli_affected_rows($conn) > 0) {
+    $output['success'] = true;
+} else {
+    $output['errors'][] = 'Error saving item to database';
+}
+
+?>
+
